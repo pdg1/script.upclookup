3016db114af580a56a2290a8926d92cafa593094
pdg1 <ryan.pdg1@gmail.com>
2019-03-21 19:23:16 -0400

    changing how lookup is handled. No longer using CSV file. instead using SQlite. <if anyone is reading this, please know that I'm an idiot and this is my learning project>

--- lookup.py
+++ lookup.py
@@ -15,10 +15,15 @@
 
 
 import csv, os, errno
+import sqlite3 as dbase
 
-dvd_csv = "resources/dvd_csv_short.csv" # currently short list allow user to input path in UI. input("enter file path")
-dvd_search_result = "resources/dvd_search_result.csv"  # output file of search results
-scan_movie_upc = "resources/upconly_test.csv" # NEW OLD UPC LIST "resources/history_upconly.csv" # individual UPC used originally '669198800047'
+# Define variables to be used
+user_db = dbase.connect('user.db') # connect to or create user database for scanned/imported UPC's
+disc_db = dbase.connect('dvd_list.db') #connect to list of physical media for the purpose of UPC crossover
+
+# REMOVE NO LONGER USING CSV # dvd_csv = "resources/dvd_csv_short.csv" # currently short list allow user to input path in UI. input("enter file path")
+# dvd_search_result = "resources/dvd_search_result.csv"  # output file of search results
+# scan_movie_upc = "resources/upconly_test.csv" # NEW OLD UPC LIST "resources/history_upconly.csv" # individual UPC used originally '669198800047'
 movies_directory = "resources/temp/movies" # movie library directory variable
 directory_to_add = "test" # movie folder variable
 
@@ -27,30 +32,36 @@ csv_file = csv.reader(open(dvd_csv, "rb"), delimiter=",")
 scanned_upc_list = csv.reader(open(scan_movie_upc, "rb"), delimiter=",")
 upc_input = raw_input('Enter number to find\n')
 
+class lookup:
+    "UPC Lookup class"
+    def __init__(self):
+        self.upc_in = upc_in
+        self.disc_title = disc_title
+
+    def makedirectory():
+        try:
+            os.makedirs(movies_directory + "/" + directory_to_add)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
 
 # Define function to create directory. todo add argument for movie ID
-def makedirectory():
-    try:
-        os.makedirs(movies_directory + "/" + directory_to_add)
-    except OSError as e:
-        if e.errno != errno.EEXIST:
-            raise
+
+# REMOVE NO LONGER USING CSV. USING SQLITE DB # Define function with argument upc(row[11]) returns movie title (row[0])
+# def csv_search(upc):
+#     # input number you want to search ( has been replaced with upc arg )
+#     # loop through csv list
+#     for row in csv_file:
+#         # print row
+#         # if current rows 12th value is equal to input, print that row and return it
+#         if row[11] == upc:
+#             print "some found"
+#             print row[0]
+#             return row[0]  # ISSUE1
 
 
-# Define function with argument upc(row[11]) returns movie title (row[0])
-def csv_search(upc):
-    # input number you want to search ( has been replaced with upc arg )
-    # loop through csv list
-    for row in csv_file:
-        # print row
-        # if current rows 12th value is equal to input, print that row and return it
-        if row[11] == upc:
-            print "some found"
-            print row[0]
-            return row[0]  # ISSUE1
-
-# Define function with argument title (returned movie title from csv_search)
-def mk_file(title):
+def mk_file(title): # Define function with argument title (returned movie title from csv_search)
     # make file at file path indicated with argument title
     make_file = open('C:\Users\Ryan\Desktop\movies\%s.dvd.disk' % title, 'w')
     # information to enter into file
@@ -60,33 +71,4 @@ def mk_file(title):
     make_file.close()
 
 
-# This works?
 mk_file(csv_search(upc_input))
-
-# This doesn't
-# for number in scanned_upc_list:
-#     mk_file(csv_search(number))
-
-
-
-# with open(scan_movie_upc, 'rb') as scanned:
-#     reader_scanned = csv.reader(scanned, delimiter=',')
-#     for upc in reader_scanned:
-#         print upc
-# with open(dvd_csv, 'rb') as f, open(dvd_search_result, 'wb') as outf, open(scan_movie_upc, 'rb') as scanned:
-#             reader = csv.reader(f, delimiter=',')
-#             writer = csv.writer(outf)
-#             # reader_scanned = csv.reader(scanned, delimiter=',')
-#             for upc in reader_scanned:
-#                 # print upc
-#                 for row in reader:
-#                     print upc
-#                     # print row[11]
-#                     if row[11] == upc[0]:
-#                         print ('Found: {}'.format(row))
-#                         print ('{}'.format(row[0]))
-#                         # num_found = num_found +   1
-#                         # print num_found
-#                         #  writer.writerow(row)
-
-# print "%d match(es) found" % (num_found,)
