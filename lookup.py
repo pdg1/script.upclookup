aab644c92cd0e7297c81ab1503a05507dbf99da0
pdg1 <ryan.pdg1+github@gmail.com>
2019-04-30 21:44:06 -0700

    Adding resource: DVD list database, free for non commercial uses. link in copyright.txt

--- lookup.py
+++ lookup.py
@@ -21,14 +21,10 @@ import sqlite3 as dbase
 user_db = dbase.connect('user.db') # connect to or create user database for scanned/imported UPC's
 disc_db = dbase.connect('dvd_list.db') #connect to list of physical media for the purpose of UPC crossover
 
-# REMOVE NO LONGER USING CSV # dvd_csv = "resources/dvd_csv_short.csv" # currently short list allow user to input path in UI. input("enter file path")
-# dvd_search_result = "resources/dvd_search_result.csv"  # output file of search results
-# scan_movie_upc = "resources/upconly_test.csv" # NEW OLD UPC LIST "resources/history_upconly.csv" # individual UPC used originally '669198800047'
 movies_directory = "resources/temp/movies" # movie library directory variable
 directory_to_add = "test" # movie folder variable
 
 # read csv, and split on "," the line
-csv_file = csv.reader(open(dvd_csv, "rb"), delimiter=",")
 scanned_upc_list = csv.reader(open(scan_movie_upc, "rb"), delimiter=",")
 upc_input = raw_input('Enter number to find\n')
 
@@ -48,19 +44,6 @@ class lookup:
 
 # Define function to create directory. todo add argument for movie ID
 
-# REMOVE NO LONGER USING CSV. USING SQLITE DB # Define function with argument upc(row[11]) returns movie title (row[0])
-# def csv_search(upc):
-#     # input number you want to search ( has been replaced with upc arg )
-#     # loop through csv list
-#     for row in csv_file:
-#         # print row
-#         # if current rows 12th value is equal to input, print that row and return it
-#         if row[11] == upc:
-#             print "some found"
-#             print row[0]
-#             return row[0]  # ISSUE1
-
-
 def mk_file(title): # Define function with argument title (returned movie title from csv_search)
     # make file at file path indicated with argument title
     make_file = open('C:\Users\Ryan\Desktop\movies\%s.dvd.disk' % title, 'w')
@@ -69,6 +52,3 @@ def mk_file(title): # Define function with argument title (returned movie title 
         "<discstub> <title> No location available</title>    <message> No location was given.</message>  </discstub>")
     # close the file
     make_file.close()
-
-
-mk_file(csv_search(upc_input))
